3c90d151151e58a6022159e00500c5c42be2f231
A Joseph <alecrewinkel@gmail.com>
2021-10-08 11:56:13 -0600

    connections

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express();
 ////http.createServer method turns your computer into a http server object. Can listen to ports on your computer and execute a function, a requestListener, each time a request is made
 const server = http.createServer(app);
 const io = socketIo(server);
+
+// Set the static folder so that you can access the frontend
+//__dirname= current directory 
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'Sport Chat Bot!'
 // Run when a client connects
 //// .on() listens for some kind of event
 io.on('connection', socket => {
@@ -17,7 +23,7 @@ io.on('connection', socket => {
 
     ////socket.emit() sends a message to all the connected clients
     //Welcomes current user
-    socket.emit('message', 'welcome to the SportsChat')
+    socket.emit('message', formatMessage(botName, 'welcome to the SportsChat'))
 
     //// this lets all clients except the client connecting that someone has connected
    //Broadcasts when a user connects
@@ -35,9 +41,7 @@ io.on('connection', socket => {
     })
 });
 
-// Set the static folder so that you can access the frontend
-//__dirname= current directory 
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 const PORT = 3000 || process.env.PORT
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
